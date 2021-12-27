2d38ab4aad0cb027adce970d25bd27d761db2c54
costab <costa.basil@gmail.com>
2021-12-27 00:25:48 -0800

    Added the DbUtils module that contains the F# version of the TempDataLoader from the facil runtime and a function that does the bulk insert. I also added a test function where I show the separation between the insertion of the data in the temp table and the querying of the temp table.

---
+++ TestTempTable/DbUtils.fs
@@ -0,0 +1,67 @@
+﻿module DbUtils
+
+open System.Data
+open Facil.Runtime.CSharp
+open Microsoft.Data.SqlClient
+
+type BulkCopyTempDataLoader(fieldCount: int, data: seq<obj array>) =
+
+    let enumerator = data.GetEnumerator()
+
+    interface IDataReader  with
+      member x.Read() = enumerator.MoveNext()
+      member x.FieldCount with get() = fieldCount
+      member x.GetValue i = enumerator.Current.[i]
+      
+      member x.Close() = failwith "not implemented"
+      member x.Dispose() = ()
+      member x.GetBoolean i = failwith "not implemented"
+      member x.GetByte i = failwith "not implemented"
+      member x.GetBytes(i, fieldOffset, buffer, bufferoffset, length) = failwith "not implemented"
+      member x.GetChar i = failwith "not implemented"
+      member x.GetChars(i, fieldOffset, buffer, bufferoffset, length) = failwith "not implemented"
+      member x.GetData i = failwith "not implemented"
+      member x.GetDataTypeName i = failwith "not implemented"
+      member x.GetDateTime i = failwith "not implemented"
+      member x.GetDecimal i = failwith "not implemented"
+      member x.GetDouble i = failwith "not implemented"
+      member x.GetFieldType i = failwith "not implemented"
+      member x.GetFloat i = failwith "not implemented"
+      member x.GetGuid i = failwith "not implemented"
+      member x.GetInt16 i = failwith "not implemented"
+      member x.GetInt32 i = failwith "not implemented"
+      member x.GetInt64 i = failwith "not implemented"
+      member x.GetName i = failwith "not implemented"
+      member x.GetOrdinal name = failwith "not implemented"
+      member x.GetSchemaTable() = failwith "not implemented"
+      member x.GetString i = failwith "not implemented"
+      
+      member x.GetValues values = failwith "not implemented"
+      member x.IsDBNull i = failwith "not implemented"
+      member x.NextResult() = failwith "not implemented"
+      member x.Depth with get() = 0
+      member x.IsClosed with get() = failwith "not implemented"
+      member x.RecordsAffected with get() = failwith "not implemented"
+      member x.Item
+          with get (name: string) : obj = failwith "not implemented"
+      member x.Item
+          with get (i: int) : obj = failwith "not implemented"
+
+let bulkCopyData (cn: SqlConnection) (tran: SqlTransaction) (configCmd: SqlCommand -> unit) (tempTableData: #seq<TempTableData>) =
+    tempTableData
+    |> Seq.iter (fun data ->
+      
+      use cmd = cn.CreateCommand()
+      configCmd cmd
+      
+      cmd.CommandText <- data.Definition
+      cmd.ExecuteNonQuery() |> ignore
+      
+      use bulkCopy = new SqlBulkCopy (cn, SqlBulkCopyOptions.Default, tran)
+      bulkCopy.DestinationTableName <- data.DestinationTableName
+      bulkCopy.BatchSize <- 10000
+      
+      use reader = new BulkCopyTempDataLoader(data.NumFields, data.Data)
+      bulkCopy.WriteToServer(reader)
+      
+    )            
--- TestTempTable/Tests.fs
+++ TestTempTable/Tests.fs
@@ -1,33 +1,95 @@
 module Tests
 
 open System
+
+open System.Collections.Generic
+open Facil.Runtime.CSharp
 open Microsoft.Data.SqlClient
 open Xunit
+open DbUtils
 
 open DbGen
 
 // change the connection string to point to your db
 let connectionStr = "Server=localhost; Database=Test; Trusted_Connection=true; TrustServerCertificate=True"
 
-
-[<Fact>]
-let ``Handling temp tables within a sql transaction should work`` () =
-    use cn = new SqlConnection(connectionStr)
+let createAndOpenConnection () =
+    let cn = new SqlConnection(connectionStr)
     cn.Open()
-    use tran = cn.BeginTransaction()
+    cn
     
-    let rows = seq { for i in 1 .. 100 do Scripts.SelectFromTempTable.TempTable.create(i, Some $"Item {i}") }
-    
-    let result =
-        Scripts.SelectFromTempTable
-            .WithConnection(cn)
-            .WithParameters(rows)
-            .Execute()
-    
-    result.ToArray() |> Array.iter (printfn "%A")
-    
+let defaultCmdConfig (tran: SqlTransaction) = 
+    fun (cmd:SqlCommand) -> 
+        cmd.CommandTimeout <- 0
+        if tran <> null then
+          cmd.Transaction <- tran
+          
+let createTempData () = seq { for i in 1 .. 10000 do Scripts.SelectFromTempTable.TempTable.create(i, Some $"Item {i}") }
+
+let executeWithinTransaction (action: SqlConnection -> SqlTransaction -> unit) =
+    use cn = createAndOpenConnection ()    
+    use tran = cn.BeginTransaction()    
+
+    action cn tran    
     tran.Commit()
     
     ()
     
+
+[<Fact>]
+let ``Handling temp tables within a sql transaction should work`` () =
+
+    let action cn tran = 
+        let rows = createTempData () 
+        
+        let result =
+            Scripts.SelectFromTempTable                
+                .WithConnection(cn)
+                .ConfigureCommand(defaultCmdConfig tran)
+                .WithParameters(rows)
+                .Execute()
     
+        result.ToArray() |> Array.iter (printfn "%A")
+    
+    executeWithinTransaction action
+    
+    ()
+    
+[<Fact>]
+let ``Handling temp tables within a sql transaction should work - fix`` () =
+    let action cn tran = 
+        let tempRows = createTempData () 
+
+        // It would be nice to give the developer the ability to separate the insertion of the data in the temp table
+        // from the selection from the temp or temp tables                 
+        Scripts.SelectFromTempTable
+            .WithConnection(cn)
+            .ConfigureCommand(defaultCmdConfig tran)
+            .CreateTempTableData(tempRows)
+        |> bulkCopyData cn tran (defaultCmdConfig(tran))
+            
+        // Unfortunately there is no way to read back the data from the temp table using the facil generated code because
+        // it tries to insert the temp table data. let's use ado.net
+        use cmd = new SqlCommand("select * from #TempTable order by id", cn, tran)
+        use sdr = cmd.ExecuteReader()
+        let tempRowsInserted = new ResizeArray<Scripts.SelectFromTempTable_Result>()
+        while sdr.Read() do
+           tempRowsInserted.Add ({id = sdr.GetInt32(0); description = Some (sdr.GetString(1)) }) 
+        
+        //printfn "%i" tempRowsInserted.Count
+        tempRowsInserted |> Seq.iter (printfn "%A")
+        Assert.Equal(tempRows |> Seq.length, tempRowsInserted.Count)
+        tempRows
+            |> Seq.iteri(fun i it ->
+                Assert.True(it.Fields.[0] = tempRowsInserted.[i].id
+                            && it.Fields.[1] = tempRowsInserted.[i].description.Value))
+            
+
+        ()
+            
+    executeWithinTransaction action
+    
+    
+    
+    ()
+           
